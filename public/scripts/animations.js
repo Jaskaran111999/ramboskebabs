40a30c319079737cd7623d7387ed7ce81a4beb81
Jaskaran111999 <jaskaran110398@gmail.com>
2019-01-12 22:42:27 +1100

    Added Navbar Overlay animation

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -1,9 +1,12 @@
+var nav = document.getElementById('nav');
 var navIcon = document.getElementById('nav-icon');
+var navOverlay = document.getElementById("nav-overlay");
 var mySidenav = document.getElementById("mySidenav");
 
 function animateNav() {
-	navIcon.classList.toggle("open");	
-	mySidenav.classList.toggle("sidenav-opened");
+	navIcon.classList.toggle("open");
+	navOverlay.classList.toggle("nav-overlay-expanded");
+	//mySidenav.classList.toggle("sidenav-opened");
 }
 
 function animate() {
