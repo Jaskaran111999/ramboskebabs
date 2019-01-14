ce72e39491b06e16a4952e0133cc032954d36faf
Jaskaran111999 <jaskaran110398@gmail.com>
2019-01-14 20:29:13 +1100

    Completed nav animation

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -2,15 +2,36 @@ var nav = document.getElementById('nav');
 var navIcon = document.getElementById('nav-icon');
 var navOverlay = document.getElementById("nav-overlay");
 var mySidenav = document.getElementById("mySidenav");
+var sidenavLinks = mySidenav.children;
+
+//milliseconds
+var navOverlayDuration = 300;
 
 function animateNav() {
 	navIcon.classList.toggle("open");
 	navOverlay.classList.toggle("nav-overlay-expanded");
-	//mySidenav.classList.toggle("sidenav-opened");
+
+	if(mySidenav.classList[1] == null) {
+		setTimeout(function () {
+			mySidenav.classList.add("sidenav-opened");
+			for(var i = 0; i < sidenavLinks.length; i++) {
+				sidenavLinks[i].style.right = "0%";
+			}
+		}, navOverlayDuration/2);
+	}
+
+	if(mySidenav.classList[1] == "sidenav-opened") {
+		setTimeout(function () {
+			mySidenav.classList.remove("sidenav-opened");
+			for(var i = 0; i < sidenavLinks.length; i++) {
+				sidenavLinks[i].style.right = "-100%";
+			}
+		}, navOverlayDuration/2);
+	}
 }
 
 function animate() {
 	nav.addEventListener('click', animateNav);
 }
 
-window.onload = animate();
+window.nload = animate();
