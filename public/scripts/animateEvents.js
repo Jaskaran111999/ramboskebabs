a5df7fb8344d0c4387d66b73b33f35dfb083c0a5
Jaskaran111999 <jaskaran110398@gmail.com>
2019-07-03 05:27:42 +1000

    event scrolling

--- public/scripts/animateEvents.js
+++ public/scripts/animateEvents.js
@@ -9,6 +9,16 @@ var videoCtn = document.getElementById("video-ctn");
 var videos = document.getElementsByClassName("event-video");
 var closeBtn = document.getElementsByClassName("close")[0];
 
+const _C = document.querySelector('#event-bg'), 
+  N = _C.children.length, NF = 30, 
+  TFN = {
+    'bounce-out': function(k, a = 2.75, b = 1.5) {
+      return 1 - Math.pow(1 - k, a)*Math.abs(Math.cos(Math.pow(k, b)*(n + .5)*Math.PI))
+      }
+  };
+
+let i = 0, y0 = null, locked = false, w, ini, fin, rID = null, anf, n;
+
 function closeVideo(e) {
   
   //hide overlay and video-ctn
@@ -62,11 +72,75 @@ function animateEvents(e) {
 
 }
 
+function stopAni() {
+  cancelAnimationFrame(rID);
+  rID = null
+};
+
+function ani(cf = 0) {
+  _C.style.setProperty('--i', ini + (fin - ini)*TFN['bounce-out'](cf/anf));
+
+  if(cf === anf) {
+    stopAni();
+    return
+  }
+
+  rID = requestAnimationFrame(ani.bind(this, ++cf))
+};
+
+function unify(e) {
+  return e.changedTouches ? e.changedTouches[0] : e
+};
+
+function lock(e) {
+  e.preventDefault();
+  y0 = unify(e).clientY;
+	locked = true
+};
+
+function drag(e) {
+  e.preventDefault();
+	
+  if(locked) {
+    let dy = unify(e).clientY - y0, f = +(dy/w).toFixed(2);
+		
+    _C.style.setProperty('--i', i - f)
+  }
+};
+
+function move(e) {
+  e.preventDefault();
+  if(locked) {
+    let dy = unify(e).clientY - y0, 
+        s = Math.sign(dy), 
+        f = +(s*dy/w).toFixed(2);
+		
+    ini = i - s*f;
+
+    if((i > 0 || s < 0) && (i < N - 1 || s > 0) && f > .2) {
+      i -= s;
+      f = 1 - f
+    }
+
+    fin = i;
+		anf = Math.round(f*NF);
+		n = 2 + Math.round(f)
+    ani();
+    y0 = null;
+    locked = false;
+  }
+};
+
+function size() {
+  w = window.innerHeight
+};
+
 function animate() {
 
   //for first time
-  activePoster.addEventListener('click', animateVideo);
+  //activePoster.addEventListener('click', animateVideo);
 
+  /*
   //add event listeners on timeline events 
   for(var i = 0; i < timelineEvents.length ; i++) {
     timelineEvents[i].addEventListener('click', function(e) {
@@ -74,6 +148,23 @@ function animate() {
       animateEvents(e);
     }, false);
   }
+  */
+
+
+
+  size();
+  _C.style.setProperty('--n', N);
+
+  addEventListener('resize', size, false);
+
+  _C.addEventListener('mousedown', lock, false);
+  _C.addEventListener('touchstart', lock, false);
+
+  _C.addEventListener('mousemove', drag, false);
+  _C.addEventListener('touchmove', drag, false);
+
+  _C.addEventListener('mouseup', move, false);
+  _C.addEventListener('touchend', move, false);
 
 }
 
