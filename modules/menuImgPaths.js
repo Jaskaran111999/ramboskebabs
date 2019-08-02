577c4593e96f74b97ffd8ba4d18dbc31bfa7e293
Jaskaran111999 <jaskaran110398@gmail.com>
2019-08-03 07:02:58 +1000

    Added more menu pics

--- modules/menuImgPaths.js
+++ modules/menuImgPaths.js
@@ -45,7 +45,20 @@ var menuImgPaths = {
       }
     },
 
-    fKebab: '../images/img-kebab-falafel.jpg',
+    fKebab: {
+      alt: 'Falafel Kebab', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 548px, (max-width: 900px) 791px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-kebab-falafel_alspdp_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-kebab-falafel_alspdp_c_scale,w_190.webp 190w, ../images/menu/img-kebab-falafel_alspdp_c_scale,w_548.webp 548w, ../images/menu/img-kebab-falafel_alspdp_c_scale,w_791.webp 791w, ../images/menu/img-kebab-falafel_alspdp_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 570px, (max-width: 900px) 820px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-kebab-falafel_nrhyu7_c_scale,w_1000.png',
+        srcset: ' ../images/menu/img-kebab-falafel_nrhyu7_c_scale,w_190.png 190w, ../images/menu/img-kebab-falafel_nrhyu7_c_scale,w_570.png 570w, ../images/menu/img-kebab-falafel_nrhyu7_c_scale,w_820.png 820w, ../images/menu/img-kebab-falafel_nrhyu7_c_scale,w_1000.png 1000w'
+      }
+    },
+
     lKebabMeal: '../images/img-kebab-lamb-meal.jpg',
     cKebabMeal: '../images/img-kebab-chicken-meal.jpg'
   }, snackpackImgPaths: {
@@ -65,6 +78,64 @@ var menuImgPaths = {
         srcset: '../images/menu/img-snackpack-falafel_tmzdndj_c_scale,w_969.png 969w, ../images/menu/img-snackpack-falafel_tmzdndj_c_scale,w_800.png 800w, ../images/menu/img-snackpack-falafel_tmzdndj_c_scale,w_558.png 558w, ../images/menu/img-snackpack-falafel_tmzdndj_c_scale,w_200.png 200w'
       }
     }
+  }, saladpackImgPaths: {
+  
+    lSaladpack: {
+      alt: 'Lamb Saladpack', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 536px, (max-width: 935px) 780px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-saladpack-lamb_ql7o0b_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-saladpack-lamb_ql7o0b_c_scale,w_190.webp 190w, ../images/menu/img-saladpack-lamb_ql7o0b_c_scale,w_536.webp 536w, ../images/menu/img-saladpack-lamb_ql7o0b_c_scale,w_780.webp 780w, ../images/menu/img-saladpack-lamb_ql7o0b_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 558px, (max-width: 935px) 801px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-saladpack-lamb_x8pyyt_c_scale,w_1000.png',
+        srcset: '../images/menu/img-saladpack-lamb_x8pyyt_c_scale,w_190.png 190w, ../images/menu/img-saladpack-lamb_x8pyyt_c_scale,w_558.png 558w, ../images/menu/img-saladpack-lamb_x8pyyt_c_scale,w_801.png 801w, ../images/menu/img-saladpack-lamb_x8pyyt_c_scale,w_1000.png 1000w'
+      }
+    },
+
+    cSaladpack: {
+      alt: 'Chicken Saladpack', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 538px, (max-width: 935px) 788px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-saladpack-chicken_srnpws_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-saladpack-chicken_srnpws_c_scale,w_190.webp 190w, ../images/menu/img-saladpack-chicken_srnpws_c_scale,w_538.webp 538w, ../images/menu/img-saladpack-chicken_srnpws_c_scale,w_788.webp 788w, ../images/menu/img-saladpack-chicken_srnpws_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 555px, (max-width: 935px) 800px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-saladpack-chicken_yve6pg_c_scale,w_1000.png',
+        srcset: '../images/menu/img-saladpack-chicken_yve6pg_c_scale,w_190.png 190w, ../images/menu/img-saladpack-chicken_yve6pg_c_scale,w_555.png 555w, ../images/menu/img-saladpack-chicken_yve6pg_c_scale,w_800.png 800w, ../images/menu/img-saladpack-chicken_yve6pg_c_scale,w_1000.png 1000w'
+      }
+    },
+
+    mSaladpack: {
+      alt: 'Mix Saladpack', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 537px, (max-width: 935px) 781px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-saladpack-mix_douomc_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-saladpack-mix_douomc_c_scale,w_190.webp 190w, ../images/menu/img-saladpack-mix_douomc_c_scale,w_537.webp 537w, ../images/menu/img-saladpack-mix_douomc_c_scale,w_781.webp 781w, ../images/menu/img-saladpack-mix_douomc_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 554px, (max-width: 935px) 800px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-saladpack-mix_u4ohgj_c_scale,w_1000.png',
+        srcset: '../images/menu/img-saladpack-mix_u4ohgj_c_scale,w_190.png 190w, ../images/menu/img-saladpack-mix_u4ohgj_c_scale,w_554.png 554w, ../images/menu/img-saladpack-mix_u4ohgj_c_scale,w_800.png 800w, ../images/menu/img-saladpack-mix_u4ohgj_c_scale,w_1000.png 1000w'
+      }
+    }, 
+
+    fSaladpack: {
+      alt: 'Falafel Saladpack', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 545px, (max-width: 935px) 790px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-saladpack-falafel_mzyok1_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-saladpack-falafel_mzyok1_c_scale,w_190.webp 190w, ../images/menu/img-saladpack-falafel_mzyok1_c_scale,w_545.webp 545w, ../images/menu/img-saladpack-falafel_mzyok1_c_scale,w_790.webp 790w, ../images/menu/img-saladpack-falafel_mzyok1_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 564px, (max-width: 935px) 801px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-saladpack-falafel_gxtcv5_c_scale,w_1000.png',
+        srcset: '../images/menu/img-saladpack-falafel_gxtcv5_c_scale,w_190.png 190w, ../images/menu/img-saladpack-falafel_gxtcv5_c_scale,w_564.png 564w, ../images/menu/img-saladpack-falafel_gxtcv5_c_scale,w_801.png 801w, ../images/menu/img-saladpack-falafel_gxtcv5_c_scale,w_1000.png 1000w'
+      }
+    }
+  
   }, ricepackImgPaths: {
 
     lRicepack: {
@@ -114,7 +185,6 @@ var menuImgPaths = {
     },
 
     fRicepack: {
-    
       alt: 'Falafel Ricepack', 
       webp: {
         sizes: '(max-width: 375px) 240px, (max-width: 775px) 534px, (max-width: 935px) 765px, (max-width: 969px) 960px, 960px', 
@@ -129,9 +199,107 @@ var menuImgPaths = {
 
     }
   }, meatpackImgPaths: {
-    lMeatpack: '../images/img-meatpack-lamb.jpg',
-    cMeatpack: '../images/img-meatpack-chicken.jpg',
-    mMeatpack: '../images/img-meatpack-mix.jpg',
+
+    lMeatpack: {
+      alt: 'Lamb Meatpack', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 534px, (max-width: 935px) 788px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-meatpack-chicken_kjaczd_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-meatpack-lamb_tont6r_c_scale,w_190.webp 190w, ../images/menu/img-meatpack-lamb_tont6r_c_scale,w_534.webp 534w, ../images/menu/img-meatpack-lamb_tont6r_c_scale,w_788.webp 788w, ../images/menu/img-meatpack-lamb_tont6r_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 557px, (max-width: 935px) 800px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-meatpack-chicken_q6zisb_c_scale,w_1000.png',
+        srcset: '../images/menu/img-meatpack-lamb_fczp6m_c_scale,w_190.png 190w, ../images/menu/img-meatpack-lamb_fczp6m_c_scale,w_557.png 557w, ../images/menu/img-meatpack-lamb_fczp6m_c_scale,w_800.png 800w, ../images/menu/img-meatpack-lamb_fczp6m_c_scale,w_1000.png 1000w'
+      }
+    },
+
+    cMeatpack: {
+      alt: 'Chicken Meatpack', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 541px, (max-width: 935px) 788px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-meatpack-chicken_kjaczd_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-meatpack-chicken_kjaczd_c_scale,w_190.webp 190w, ../images/menu/img-meatpack-chicken_kjaczd_c_scale,w_541.webp 541w, ../images/menu/img-meatpack-chicken_kjaczd_c_scale,w_788.webp 788w, ../images/menu/img-meatpack-chicken_kjaczd_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 569px, (max-width: 935px) 819px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-meatpack-chicken_q6zisb_c_scale,w_1000.png',
+        srcset: '../images/menu/img-meatpack-chicken_q6zisb_c_scale,w_190.png 190w, ../images/menu/img-meatpack-chicken_q6zisb_c_scale,w_569.png 569w, ../images/menu/img-meatpack-chicken_q6zisb_c_scale,w_819.png 819w, ../images/menu/img-meatpack-chicken_q6zisb_c_scale,w_1000.png 1000w'
+      }
+    },
+
+    mMeatpack: {
+      alt: 'Mix Meatpack', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 539px, (max-width: 935px) 788px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-meatpack-mix_sx9mav_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-meatpack-mix_sx9mav_c_scale,w_190.webp 190w, ../images/menu/img-meatpack-mix_sx9mav_c_scale,w_539.webp 539w, ../images/menu/img-meatpack-mix_sx9mav_c_scale,w_788.webp 788w, ../images/menu/img-meatpack-mix_sx9mav_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 597px, (max-width: 935px) 828px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-meatpack-mix_byzg2r_c_scale,w_1000.png',
+        srcset: '../images/menu/img-meatpack-mix_byzg2r_c_scale,w_190.png 190w, ../images/menu/img-meatpack-mix_byzg2r_c_scale,w_597.png 597w, ../images/menu/img-meatpack-mix_byzg2r_c_scale,w_828.png 828w, ../images/menu/img-meatpack-mix_byzg2r_c_scale,w_1000.png 1000w'
+      }
+    }
+
+  }, platterImgPaths: {
+
+    lPlatter: {
+      alt: 'Lamb Platter', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 524px, (max-width: 935px) 772px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-platter-lamb_fhfalk_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-platter-lamb_fhfalk_c_scale,w_190.webp 190w, ../images/menu/img-platter-lamb_fhfalk_c_scale,w_524.webp 524w, ../images/menu/img-platter-lamb_fhfalk_c_scale,w_772.webp 772w, ../images/menu/img-platter-lamb_fhfalk_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 551px, (max-width: 935px) 791px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-platter-lamb_kxrbej_c_scale,w_1000.png',
+        srcset: '../images/menu/img-platter-lamb_kxrbej_c_scale,w_190.png 190w, ../images/menu/img-platter-lamb_kxrbej_c_scale,w_551.png 551w, ../images/menu/img-platter-lamb_kxrbej_c_scale,w_791.png 791w, ../images/menu/img-platter-lamb_kxrbej_c_scale,w_1000.png 1000w'
+      }
+    },
+
+    cPlatter: {
+      alt: 'Chicken Platter', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 536px, (max-width: 935px) 783px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-platter-chicken_d2rvke_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-platter-chicken_d2rvke_c_scale,w_190.webp 190w, ../images/menu/img-platter-chicken_d2rvke_c_scale,w_536.webp 536w, ../images/menu/img-platter-chicken_d2rvke_c_scale,w_783.webp 783w, ../images/menu/img-platter-chicken_d2rvke_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 560px, (max-width: 935px) 804px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-platter-chicken_wknwzr_c_scale,w_1000.png',
+        srcset: '../images/menu/img-platter-chicken_wknwzr_c_scale,w_190.png 190w, ../images/menu/img-platter-chicken_wknwzr_c_scale,w_560.png 560w, ../images/menu/img-platter-chicken_wknwzr_c_scale,w_804.png 804w, ../images/menu/img-platter-chicken_wknwzr_c_scale,w_1000.png 1000w'
+      }
+    },
+
+    mPlatter: {
+      alt: 'Mix Platter', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 540px, (max-width: 935px) 789px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-platter-mix_wufjjb_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-platter-mix_wufjjb_c_scale,w_190.webp 190w, ../images/menu/img-platter-mix_wufjjb_c_scale,w_540.webp 540w, ../images/menu/img-platter-mix_wufjjb_c_scale,w_789.webp 789w, ../images/menu/img-platter-mix_wufjjb_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 559px, (max-width: 935px) 801px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-platter-mix_bq1u6o_c_scale,w_1000.png',
+        srcset: '../images/menu/img-platter-mix_bq1u6o_c_scale,w_190.png 190w, ../images/menu/img-platter-mix_bq1u6o_c_scale,w_559.png 559w, ../images/menu/img-platter-mix_bq1u6o_c_scale,w_801.png 801w, ../images/menu/img-platter-mix_bq1u6o_c_scale,w_1000.png 1000w'
+      }
+    }, 
+
+    vPlatter: {
+      alt: 'Vegetarian Platter', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 536px, (max-width: 935px) 844px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-platter-veg_arnu7f_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-platter-veg_arnu7f_c_scale,w_190.webp 190w, ../images/menu/img-platter-veg_arnu7f_c_scale,w_563.webp 563w, ../images/menu/img-platter-veg_arnu7f_c_scale,w_844.webp 844w, ../images/menu/img-platter-veg_arnu7f_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 601px, (max-width: 935px) 845px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-platter-veg_hg60vv_c_scale,w_1000.png',
+        srcset: '../images/menu/img-platter-veg_hg60vv_c_scale,w_190.png 190w, ../images/menu/img-platter-veg_hg60vv_c_scale,w_601.png 601w, ../images/menu/img-platter-veg_hg60vv_c_scale,w_845.png 845w, ../images/menu/img-platter-veg_hg60vv_c_scale,w_1000.png 1000w'
+      }
+    }
+
   }, appetizerImgPaths: {
     gozleme: '../images/img-appetizer-gozleme.jpg',
     chips: '../images/img-appetizer-chips.jpg'
@@ -150,8 +318,35 @@ var menuImgPaths = {
   }, addonImgPaths: {
     cheese: '../images/img-addon-cheese.jpg',
     pChillies: '../images/img-addon-pickeledChillies.jpg',
-    hommus: '../images/img-addon-hommus.jpg',
-    tzatziki: '../images/img-addon-tzatziki.jpg',
+
+    hommus: {
+      alt: 'Hommus Image', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 539px, (max-width: 935px) 784px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-addon-hommus_zdazw0_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-addon-hommus_zdazw0_c_scale,w_190.webp 190w, ../images/menu/img-addon-hommus_zdazw0_c_scale,w_539.webp 539w, ../images/menu/img-addon-hommus_zdazw0_c_scale,w_784.webp 784w, ../images/menu/img-addon-hommus_zdazw0_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 565px, (max-width: 935px) 800px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-addon-hommus_dv4rtp_c_scale,w_1000.png',
+        srcset: '../images/menu/img-addon-hommus_dv4rtp_c_scale,w_190.png 190w, ../images/menu/img-addon-hommus_dv4rtp_c_scale,w_565.png 565w, ../images/menu/img-addon-hommus_dv4rtp_c_scale,w_800.png 800w, ../images/menu/img-addon-hommus_dv4rtp_c_scale,w_1000.png 1000w'
+      }
+    },
+
+    tzatziki: {
+      alt: 'Tzatziki Image', 
+      webp: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 540px, (max-width: 935px) 789px, (max-width: 969px) 1000px, 1000px', 
+        src: '../images/menu/img-addon-tzatziki_qdvafy_c_scale,w_1000.webp',
+        srcset: '../images/menu/img-addon-tzatziki_qdvafy_c_scale,w_190.webp 190w, ../images/menu/img-addon-tzatziki_qdvafy_c_scale,w_540.webp 540w, ../images/menu/img-addon-tzatziki_qdvafy_c_scale,w_789.webp 789w, ../images/menu/img-addon-tzatziki_qdvafy_c_scale,w_1000.webp 1000w'
+      }, 
+      png: {
+        sizes: '(max-width: 375px) 190px, (max-width: 775px) 560px, (max-width: 935px) 800px, (max-width: 969px) 1000px, 1000px',
+        src: '../images/menu/img-addon-tzatziki_ju2tt8_c_scale,w_1000.png',
+        srcset: '../images/menu/img-addon-tzatziki_ju2tt8_c_scale,w_190.png 190w, ../images/menu/img-addon-tzatziki_ju2tt8_c_scale,w_560.png 560w, ../images/menu/img-addon-tzatziki_ju2tt8_c_scale,w_800.png 800w, ../images/menu/img-addon-tzatziki_ju2tt8_c_scale,w_1000.png 1000w'
+      }
+    },
+
     chips: '../images/img-addon-chips.jpg',
     flakes: '../images/img-addon-flakes.jpg',
     extraMeat: '../images/img-addon-extraMeat.jpg',
